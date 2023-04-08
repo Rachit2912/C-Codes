e01cde5ea195113b23f48846a717cc0d42d0c565
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2023-04-08 22:51:13 +0530

    Update 001 Namaste Duniya.cpp

--- 001 Namaste Duniya.cpp
+++ 001 Namaste Duniya.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+//include is pr-processor directive which instructs pre-processor to include "iostream" header file/
+//iostream is a header file which have all standard pre-declared functions in it.
 using namespace std;
+//std namespace is used bcoz we are using all standard inputs and outputs otherwise you have to declare all the times std::cout but with help of these we have to just write cout instead of std::cout
 
 int main() {
+// int is datatype for your main function 
+// main is a function which is the actual beginning of our code
+   
         cout<< "Namaste Duniya" << endl;
-
+//cout<< is syntax for generating output in c++
+// <<endl is used for creating a new line; we can also use \'n' for this purpose
+// ; is used for terminating or ending a statement in c++
 
 }
