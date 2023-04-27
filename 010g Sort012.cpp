351d5e515cfe1b7d7e2411859bc509b8f26639f6
Rachit Joshi <114751447+Rachit2912@users.noreply.github.com>
2023-04-27 17:21:43 +0530

    Update 010g Sort012.cpp

--- 010g Sort012.cpp
+++ 010g Sort012.cpp
@@ -1,32 +1,70 @@
 //Sort 012
-//Sum Approach
+//1st Sum Approach
 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+    int s=0,o=0,n=nums.size();
+    for(int i=0;i<n;i++){
+      s+=nums[i];
+      if(nums[i]==1){o++;}
+    }
 
-//#include<stdio.h>
-void sort012(int arr[], int n)
-{
-   int s=0,o=0;
-   for(int i=0;i<n;i++){
-      s+=arr[i];
-      if(arr[i]==1){o++;}
-   }
-   printf("%d\n",s);
    int t=s-o;
    int z=n-o-(t/2);
-   printf("z is %d, o is %d, t is %d\n",z,o,t/2);
 
    for(int i=0;i<n;i++){
-      if(i<z){arr[i]=0;}
-      else if(i>=z && i<z+o){arr[i]=1;}
-      else{arr[i]=2;}
+      if(i<z){nums[i]=0;}
+      else if(i>=z && i<z+o){nums[i]=1;}
+      else{nums[i]=2;}
    }
-   
-   for(int i=0;i<n;i++){
-      printf("%d ",arr[i]);
+    }
+};
 
-      }   }
 
-int main() {
-   int arr[10]={0,1,1,2,2,0,1,1,2,0};
-    sort012(arr,10);
+
+//2nd index approach
+class Solution {
+public:
+void sortColors(vector<int>& nums) {
+vector counts(3,0);
+for(int i=0;i<nums.size();i++){
+counts[nums[i]]+=1;
 }
+int j=0;
+for(int i=0;i<3;i++){
+while(counts[i]!=0){
+nums[j] = i;
+counts[i]--;
+j++;
+}
+}
+}
+};
+
+
+
+//3rd 3pointer approach/ 3 way partitioning
+class Solution {
+public:
+
+    void sortColors(vector<int>& nums) {
+        int i=0,p=0,h=nums.size()-1;
+        while(i<=h){
+            if(nums[i]==0){
+                swap(nums[i],nums[p]);
+                p++;i++;
+            }
+
+            else if(nums[i]==2){
+                swap(nums[i],nums[h]);
+                h--;
+            }
+
+            else if(nums[i]==1){
+                i++;
+            }
+        }
+    }
+};
